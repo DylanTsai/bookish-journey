35ff78b8daa91de835dc9eb10a3ca15749a76b53
DylanTsai <dylantsai1999@gmail.com>
2020-03-04 19:53:05 -0500

    Added skeleton for Symba column test function, and commented out code that caused SQL errors

--- src/app.py
+++ src/app.py
@@ -18,4 +18,12 @@ def response():
     note = request.form.get("note") 
     return render_template("index.html", name=fname, note=note) 
 
+
+@app.route('/testRetrieveColFromSymbaApi_country')
+def test_SymbaApi_country_col():
+  # you'll get a parameter from the person who goes to the website
+  # it'll look something like /testRetrieveColFromSymbaApi_country?col='colname'
+  pass
+  return "the column, comma-separated"
+
 if __name__ == '__main__': app.run(host="0.0.0.0", port=5000)
--- src/connectToDB.py
+++ src/connectToDB.py
@@ -20,19 +20,19 @@ cur = con.cursor()
 cur.execute("""CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
     """)
 
-cur.execute("""
-    create TABLE public."SymbaApi_country" (
-    country_id uuid NOT NULL,
-    name character varying(50) NOT NULL,
-    code character varying(50),
-    description character varying(100),
-    is_active boolean NOT NULL,
-    is_deleted boolean NOT NULL,
-    created_on timestamp with time zone NOT NULL,
-    modified_on timestamp with time zone,
-    created_by uuid,
-    modifeid_by uuid);
-    """)
+# cur.execute("""
+#     create TABLE public."SymbaApi_country" (
+#     country_id uuid NOT NULL,
+#     name character varying(50) NOT NULL,
+#     code character varying(50),
+#     description character varying(100),
+#     is_active boolean NOT NULL,
+#     is_deleted boolean NOT NULL,
+#     created_on timestamp with time zone NOT NULL,
+#     modified_on timestamp with time zone,
+#     created_by uuid,
+#     modifeid_by uuid);
+#     """)
 
 # update/insert data as in sql
 cur.execute(""" 
