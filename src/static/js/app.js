759061d1f95110499855bee472cdbc8154b068cf
Sudeshna <slp256@cornell.edu>
2020-03-17 02:31:40 -0400

    Test function added to check Typescript works

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -3,14 +3,12 @@
 import React from "react";
 import { FetchTester, LikeButton } from './index.jsx'
 import { SkillSearchBox } from './optionSearchBox.jsx'
+import hello from './test'
 import ReactDOM from 'react-dom'
 import regeneratorRuntime from "regenerator-runtime";
 
-function buttontest(el) {
-  console.log("rawr");
-}
+hello();
 
 ReactDOM.render(<FetchTester />, document.getElementById("fetch-tester"));
 ReactDOM.render(<LikeButton />, document.getElementById("simple-react-test"));
 ReactDOM.render(<SkillSearchBox />, document.getElementById("skill-search-box"));
-
---
+++ src/static/js/test.tsx
@@ -0,0 +1,8 @@
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+
+export default function hello() {
+  console.log("if this works we're good to go");
+}
+
+// ReactDOM.render(React.createElement("h2", null, "Hello, world!"), document.body);
